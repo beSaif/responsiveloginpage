0a065950f0fe53787aa6932cdf236763809f281e
beSaif <hishamkolikara@gmail.com>
2020-10-03 14:23:42 +0530

    Wallet

---
+++ lib/pages/funds.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/size_config.dart';
+
+class FundsPage extends StatefulWidget {
+  @override
+  _FundsPageState createState() => _FundsPageState();
+}
+
+class _FundsPageState extends State<FundsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          elevation: 0,
+          backgroundColor: Color(0xFF0B3954),
+          brightness: Brightness.light,
+          centerTitle: true,
+          title: Text(
+            'FUNDS',
+          ),
+          automaticallyImplyLeading: false,
+        ),
+        backgroundColor: Color(0xFFf8f8ff),
+        body: Container(
+          color: Color(0xFFf8f8ff),
+          padding: EdgeInsets.only(bottom: 00),
+          width: MediaQuery.of(context).size.width,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                      color: Color(0xFF0B3954),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40))),
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Wallet Balance',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              ),
+                              Text(
+                                'INR',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 35),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '₹9483.20',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        gradient: LinearGradient(
+                            colors: [Color(0xFFe67e22), Color(0xFFf1c40f)])),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  'ADD FUNDS',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize:
+                                          SizeConfig.blockSizeHorizontal * 3.3),
+                                ),
+                              ],
+                            ),
+                          ),
+                          color: Color(0xFF4cb050),
+                          onPressed: () {},
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Expanded(
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Icon(
+                                Icons.refresh,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'WITHDRAW',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize:
+                                        SizeConfig.blockSizeHorizontal * 3.3),
+                              ),
+                            ],
+                          ),
+                          color: Color(0xFF4185f4),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loginpage/pages/funds.dart';
 import 'package:loginpage/pages/homePage.dart';
 import '../size_config.dart';
 
@@ -12,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool loadingscreen = false;
   User user;
   _HomePageState(this.user);
   @override
@@ -20,7 +22,10 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.account_balance_wallet),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => FundsPage()));
+          },
         ),
         actions: [
           IconButton(
@@ -39,8 +44,8 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       body: Container(
-        color: Color(0xFFBDC3C7),
-        padding: EdgeInsets.only(bottom: 100),
+        color: Color(0xFFf8f8ff),
+        padding: EdgeInsets.only(bottom: 00),
         width: MediaQuery.of(context).size.width,
         child: SingleChildScrollView(
           child: Column(
@@ -174,6 +179,104 @@ class _HomePageState extends State<HomePage> {
                       ),
                     )
                   ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 13),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Transaction History',
+                  style: TextStyle(fontSize: 22),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                alignment: Alignment.topLeft,
+                child: Container(
+                  child: ListView.builder(
+                      itemCount: 5,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 13),
+                          height: 76,
+                          padding: EdgeInsets.only(
+                              left: 24, top: 12, bottom: 12, right: 22),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Color(0xFFdfdfe5),
+                                    blurRadius: 10,
+                                    spreadRadius: 5,
+                                    offset: Offset(8, 8))
+                              ]),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    alignment: Alignment.center,
+                                    height: 57,
+                                    width: 57,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Color(0xFF4185f4)),
+                                    child: Text(
+                                      'B',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 13,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        "5 RC",
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontFamily: 'Montserrat',
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        '13-08-2020',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontFamily: 'Montserrat',
+                                          color: Colors.grey[10],
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '₹500',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontFamily: 'Montserrat',
+                                      color: Color(0xFF0B3954),
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      }),
                 ),
               )
             ],
